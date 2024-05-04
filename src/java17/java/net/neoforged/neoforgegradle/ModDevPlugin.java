40f00f1abe457e91d9693bdf27c3f9fecf5cf0c2
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-05-04 17:00:50 +0200

    WIP towards runClient

--- src/java17/java/net/neoforged/neoforgegradle/ModDevPlugin.java
+++ src/java17/java/net/neoforged/neoforgegradle/ModDevPlugin.java
@@ -1,20 +1,34 @@
 package net.neoforged.neoforgegradle;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 import org.gradle.api.GradleException;
 import org.gradle.api.JavaVersion;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.ModuleDependency;
 import org.gradle.api.artifacts.result.ResolvedArtifactResult;
+import org.gradle.api.file.FileTree;
+import org.gradle.api.logging.Logging;
 import org.gradle.api.plugins.JavaLibraryPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.tasks.JavaExec;
 import org.gradle.internal.component.external.model.ModuleComponentArtifactIdentifier;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
 import org.gradle.util.GradleVersion;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.zip.ZipFile;
 
 public class ModDevPlugin implements Plugin<Project> {
     private static final GradleVersion MIN_VERSION = GradleVersion.version("8.7");
@@ -129,6 +143,102 @@ public class ModDevPlugin implements Plugin<Project> {
             var javaExtension = ExtensionUtils.getExtension(project, "java", JavaPluginExtension.class);
             var toolchainSpec = javaExtension.getToolchain();
             toolchainSpec.getLanguageVersion().convention(JavaLanguageVersion.of(21));
+        }
+
+        // Let's try to get the userdev JSON out of the universal jar
+        // I don't like having to use a configuration for this...
+        var userdevJarConfiguration = configurations.create("userdevJar", spec -> {
+            spec.setCanBeResolved(true);
+            spec.setCanBeConsumed(false);
+            spec.setTransitive(false);
+            spec.withDependencies(set -> set.addLater(extension.getVersion().map(version -> dependencyFactory.create("net.neoforged:neoforge:" + version + ":userdev"))));
+        });
+        var userDevJarTree = project.provider(() -> {
+            return project.zipTree(userdevJarConfiguration.getSingleFile());
+        });
+        var userDevConfig = userDevJarTree.map(ModDevPlugin::readUserdevJson);
+
+        var legacyClasspathConfiguration = configurations.create("legacyClassPath", spec -> {
+            spec.setCanBeResolved(true);
+            spec.setCanBeConsumed(false);
+            spec.setTransitive(false);
+            spec.withDependencies(set -> set.addAllLater(userDevConfig.map(config -> {
+                return Stream.of(config.getAsJsonArray("libraries"))
+                        .flatMap(a -> a.asList().stream())
+                        .map(lib -> (Dependency) dependencyFactory.create(lib.getAsString()))
+                        .toList();
+            })));
+        });
+
+        var writeLcpTask = tasks.register("writeLegacyClasspath", WriteLegacyClasspath.class, writeLcp -> {
+            writeLcp.getEntries().from(legacyClasspathConfiguration);
+        });
+
+        tasks.register("runClient", JavaExec.class, runClientTask -> {
+            // Modules: these will be used to generate the module path
+            var modulesConfiguration = configurations.create("modules", c -> {
+                c.setCanBeConsumed(false);
+                c.setCanBeResolved(true);
+                c.withDependencies(set -> {
+                    for (var moduleDep : userDevConfig.get().getAsJsonArray("modules")) {
+                        set.add(dependencyFactory.create(moduleDep.getAsString()));
+                    }
+                });
+            });
+
+            var runs = userDevConfig.get().getAsJsonObject("runs");
+            var clientRun = runs.getAsJsonObject("client");
+
+            // This should probably all be done using providers; but that's for later :)
+            runClientTask.getMainClass().set(clientRun.get("main").getAsString());
+            for (var arg : clientRun.getAsJsonArray("args")) {
+                runClientTask.args(arg.getAsString());
+            }
+            for (var jvmArg : clientRun.getAsJsonArray("jvmArgs")) {
+                String arg = jvmArg.getAsString();
+                if (arg.equals("{modules}")) {
+                    arg = modulesConfiguration.getFiles().stream()
+                            .map(File::getAbsolutePath)
+                            .collect(Collectors.joining(File.pathSeparator));
+                }
+                runClientTask.jvmArgs(arg);
+            }
+            for (var env : clientRun.getAsJsonObject("env").entrySet()) {
+                var envValue = env.getValue().getAsString();
+                if (envValue.equals("{source_roots}")) {
+                    continue; // This is MOD_CLASSES, skip for now.
+                }
+                runClientTask.environment(env.getKey(), envValue);
+            }
+            for (var prop : clientRun.getAsJsonObject("props").entrySet()) {
+                var propValue = prop.getValue().getAsString();
+                if (propValue.equals("{minecraft_classpath_file}")) {
+                    propValue = writeLcpTask.flatMap(WriteLegacyClasspath::getLegacyClasspathFile).get().getAsFile().getAbsolutePath();
+                    runClientTask.dependsOn(writeLcpTask); // I think this is needed because Gradle can't track who called .get()? to be confirmed... I need to check that I didn't make the same mistake elsewhere
+                }
+
+                runClientTask.systemProperty(prop.getKey(), propValue);
+            }
+
+            runClientTask.classpath(configurations.named("runtimeClasspath"));
+            // Create directory if needed
+            runClientTask.doFirst(t -> {
+                try {
+                    Files.createDirectories(project.file("run/").toPath());
+                } catch (IOException e) {
+                    throw new UncheckedIOException("Failed to create run directory", e);
+                }
+            });
+            runClientTask.setWorkingDir(project.file("run/"));
+        });
+    }
+
+    private static JsonObject readUserdevJson(FileTree zf) {
+        var zipFile = zf.matching(pf -> pf.include("config.json")).getSingleFile();
+        try (var reader = Files.newBufferedReader(zipFile.toPath())) {
+            return JsonParser.parseReader(reader).getAsJsonObject();
+        } catch (IOException exception) {
+            throw new UncheckedIOException("Failed to read userdev config.json", exception);
         }
     }
 
---
+++ src/java17/java/net/neoforged/neoforgegradle/WriteLegacyClasspath.java
@@ -0,0 +1,39 @@
+package net.neoforged.neoforgegradle;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.TaskAction;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.TreeSet;
+
+abstract class WriteLegacyClasspath extends DefaultTask {
+    @Inject
+    public WriteLegacyClasspath(Project project) {
+        getLegacyClasspathFile().convention(() -> project.file("build/legacy-classpath.txt"));
+    }
+
+    @InputFiles
+    abstract ConfigurableFileCollection getEntries();
+
+    @OutputFile
+    abstract RegularFileProperty getLegacyClasspathFile();
+
+    @TaskAction
+    public void writeLegacyClasspath() throws IOException {
+        var legacyClasspath = new StringBuilder();
+        // Copy the entries to a tree set to ensure deterministic order if we have to debug classpath problems...
+        for (var entry : new TreeSet<>(getEntries().getFiles())) {
+            legacyClasspath.append(entry.getAbsolutePath()).append(System.lineSeparator());
+        }
+
+        Files.writeString(getLegacyClasspathFile().getAsFile().get().toPath(), legacyClasspath.toString(), StandardOpenOption.CREATE);
+    }
+}
