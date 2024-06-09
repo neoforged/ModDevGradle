08fade01835aaa9bba4ed9fa23c09bfe6a5952f9
Sebastian Hartte <shartte@users.noreply.github.com>
2024-06-09 12:21:14 +0200

    Refactor NFRT tasks to move more shared code into a base class.

--- src/main/java/net/neoforged/moddevgradle/internal/CreateMinecraftArtifactsTask.java
+++ src/main/java/net/neoforged/moddevgradle/internal/CreateMinecraftArtifactsTask.java
@@ -3,41 +3,20 @@ package net.neoforged.moddevgradle.internal;
 import org.gradle.api.GradleException;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.RegularFileProperty;
-import org.gradle.api.provider.Property;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.Internal;
-import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.work.DisableCachingByDefault;
 
 import javax.inject.Inject;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 @DisableCachingByDefault(because = "Implements its own caching")
-abstract class CreateMinecraftArtifactsTask extends NeoFormRuntimeTask {
+abstract class CreateMinecraftArtifactsTask extends NeoFormRuntimeEngineTask {
     @Inject
     public CreateMinecraftArtifactsTask() {
-        // When cache is disabled, the task is NEVER up-to-date to aid with debugging problems
-        getOutputs().upToDateWhen(task -> ((CreateMinecraftArtifactsTask) task).getEnableCache().get());
     }
-
-    @InputFile
-    @Optional
-    abstract RegularFileProperty getArtifactManifestFile();
-
-    /**
-     * Enable verbose NeoForm-in-a-box output
-     */
-    @Internal
-    abstract Property<Boolean> getVerbose();
-
-    @Input
-    abstract Property<String> getNeoForgeArtifact();
 
     @InputFiles
     abstract ConfigurableFileCollection getAccessTransformers();
@@ -60,35 +39,10 @@ abstract class CreateMinecraftArtifactsTask extends NeoFormRuntimeTask {
     @OutputFile
     abstract RegularFileProperty getResourcesArtifact();
 
-    /**
-     * Enables use of the cache.
-     */
-    @Internal
-    abstract Property<Boolean> getEnableCache();
-
-    @Input
-    abstract Property<Boolean> getUseEclipseCompiler();
-
-    @Input
-    abstract Property<Boolean> getAnalyzeCacheMisses();
-
     @TaskAction
-    public void createArtifacts() throws IOException {
-        var artifactId = getNeoForgeArtifact().get();
-
+    public void createArtifacts() {
         var args = new ArrayList<String>();
-        Collections.addAll(
-                args,
-                "run"
-        );
-
-        if (getVerbose().get()) {
-            args.add("--verbose");
-        }
-
-        if (!getEnableCache().get()) {
-            args.add("--disable-cache");
-        }
+        args.add("run");
 
         var accessTransformers = getAccessTransformers().getFiles();
         for (var accessTransformer : accessTransformers) {
@@ -104,30 +58,28 @@ abstract class CreateMinecraftArtifactsTask extends NeoFormRuntimeTask {
             throw new GradleException("More than one parchment data file were specified: " + parchmentData);
         }
 
-        if (getUseEclipseCompiler().get()) {
-            args.add("--use-eclipse-compiler");
-        }
-
-        if (getAnalyzeCacheMisses().get()) {
-            args.add("--analyze-cache-misses");
-        }
-
-        if (getArtifactManifestFile().isPresent()) {
-            args.add("--artifact-manifest");
-            args.add(getArtifactManifestFile().get().getAsFile().getAbsolutePath());
-            args.add("--warn-on-artifact-manifest-miss");
-        }
-
-        // NOTE: When we use NeoForm standalone, the result-ids also change
         Collections.addAll(
                 args,
-                "--neoforge", artifactId + ":userdev",
                 "--dist", "joined",
-                "--write-result", "compiledWithNeoForge:" + getCompiledArtifact().get().getAsFile().getAbsolutePath(),
-                "--write-result", "sourcesWithNeoForge:" + getSourcesArtifact().get().getAsFile().getAbsolutePath(),
-                "--write-result", "clientResources:" + getResourcesArtifact().get().getAsFile().getAbsolutePath(),
-                "--write-result", "sourcesAndCompiledWithNeoForge:" + getCompiledWithSourcesArtifact().get().getAsFile().getAbsolutePath()
+                "--write-result", "clientResources:" + getResourcesArtifact().get().getAsFile().getAbsolutePath()
         );
+
+        // NOTE: When we use NeoForm standalone, the result-ids also change
+        if (getNeoForgeArtifact().isPresent()) {
+            Collections.addAll(
+                    args,
+                    "--write-result", "compiledWithNeoForge:" + getCompiledArtifact().get().getAsFile().getAbsolutePath(),
+                    "--write-result", "sourcesWithNeoForge:" + getSourcesArtifact().get().getAsFile().getAbsolutePath(),
+                    "--write-result", "sourcesAndCompiledWithNeoForge:" + getCompiledWithSourcesArtifact().get().getAsFile().getAbsolutePath()
+            );
+        } else {
+            Collections.addAll(
+                    args,
+                    "--write-result", "compiled:" + getCompiledArtifact().get().getAsFile().getAbsolutePath(),
+                    "--write-result", "sources:" + getSourcesArtifact().get().getAsFile().getAbsolutePath(),
+                    "--write-result", "sourcesAndCompiled:" + getCompiledWithSourcesArtifact().get().getAsFile().getAbsolutePath()
+            );
+        }
 
         run(args);
     }
