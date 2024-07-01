c8ebfb722cb07e4c451f53a68a40d7daa6be3b37
shartte <shartte@users.noreply.github.com>
2024-07-01 22:07:53 +0200

    Add better support for includeBuild when creating Minecraft artifacts (#73)

--- src/main/java/net/neoforged/moddevgradle/internal/ModDevPlugin.java
+++ src/main/java/net/neoforged/moddevgradle/internal/ModDevPlugin.java
@@ -830,24 +830,35 @@ public class ModDevPlugin implements Plugin<Project> {
         String artifactId;
         String ext = "";
         String classifier = null;
+
+        var filename = result.getFile().getName();
+        var startOfExt = filename.lastIndexOf('.');
+        if (startOfExt != -1) {
+            ext = filename.substring(startOfExt + 1);
+            filename = filename.substring(0, startOfExt);
+        }
+
         if (result.getId() instanceof ModuleComponentArtifactIdentifier moduleId) {
             var artifact = moduleId.getComponentIdentifier().getModule();
             var version = moduleId.getComponentIdentifier().getVersion();
             var expectedBasename = artifact + "-" + version;
-            var filename = result.getFile().getName();
-            var startOfExt = filename.lastIndexOf('.');
-            if (startOfExt != -1) {
-                ext = filename.substring(startOfExt + 1);
-                filename = filename.substring(0, startOfExt);
-            }
 
             if (filename.startsWith(expectedBasename + "-")) {
                 classifier = filename.substring((expectedBasename + "-").length());
             }
             artifactId = moduleId.getComponentIdentifier().getGroup() + ":" + artifact + ":" + version;
         } else {
-            ext = "jar";
-            artifactId = result.getId().getComponentIdentifier().toString();
+            // When we encounter a project reference, the component identifier does not expose the group or module name.
+            // But we can access the list of capabilities associated with the published variant the artifact originates from.
+            // If the capability was not overridden, this will be the project GAV. If it is *not* the project GAV,
+            // it will be at least in valid GAV format, not crashing NFRT when it parses the manifest. It will just be ignored.
+            var capabilities = result.getVariant().getCapabilities();
+            if (capabilities.size() == 1) {
+                var capability = capabilities.get(0);
+                artifactId = capability.getGroup() + ":" + capability.getName() + ":" + capability.getVersion();
+            } else {
+                artifactId = result.getId().getComponentIdentifier().toString();
+            }
         }
         String gav = artifactId;
         if (classifier != null) {
