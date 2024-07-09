460bdd29aa7e6bf1cf12cc3028df0a32717ae16b
Matyrobbrt <matyrobbrt@gmail.com>
2024-07-10 00:00:11 +0300

    Tests

--- testproject/subproject/src/main/java/subproject/SubProject.java
+++ testproject/subproject/src/main/java/subproject/SubProject.java
@@ -1,11 +1,15 @@
 package subproject;
 
 import net.minecraft.DetectedVersion;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.fml.common.Mod;
 
 @Mod("testproject")
 public class SubProject {
     public SubProject() {
         System.out.println("Subproject: " + ((DetectedVersion) DetectedVersion.BUILT_IN).buildTime);
+
+        System.out.println(new ItemStack(Items.ACACIA_BOAT).testmodThisIsMine());
     }
 }
