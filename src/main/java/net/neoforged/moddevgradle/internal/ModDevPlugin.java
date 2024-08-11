010b26cdcec584d70cf4ff3880a0b4251056efe1
Sebastian Hartte <shartte@users.noreply.github.com>
2024-08-11 19:21:31 +0200

    Fix import

--- src/main/java/net/neoforged/moddevgradle/internal/ModDevPlugin.java
+++ src/main/java/net/neoforged/moddevgradle/internal/ModDevPlugin.java
@@ -76,6 +76,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
