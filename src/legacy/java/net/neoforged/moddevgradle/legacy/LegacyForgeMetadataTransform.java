15de0a2aac5836517ccbd64dfee571647275c32e
Matyrobbrt <matyrobbrt@gmail.com>
2024-09-07 13:31:26 +0300

    Initially split duplicate metadata transform code

--- src/legacy/java/net/neoforged/moddevgradle/legacy/LegacyForgeMetadataTransform.java
+++ src/legacy/java/net/neoforged/moddevgradle/legacy/LegacyForgeMetadataTransform.java
@@ -1,68 +1,38 @@
 package net.neoforged.moddevgradle.legacy;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.gradle.api.Action;
-import org.gradle.api.GradleException;
-import org.gradle.api.JavaVersion;
 import org.gradle.api.artifacts.ComponentMetadataContext;
-import org.gradle.api.artifacts.ComponentMetadataRule;
 import org.gradle.api.artifacts.DirectDependenciesMetadata;
 import org.gradle.api.artifacts.DirectDependencyMetadata;
 import org.gradle.api.artifacts.MutableVariantFilesMetadata;
 import org.gradle.api.artifacts.repositories.RepositoryResourceAccessor;
-import org.gradle.api.attributes.Attribute;
 import org.gradle.api.attributes.Bundling;
 import org.gradle.api.attributes.Category;
-import org.gradle.api.attributes.LibraryElements;
 import org.gradle.api.attributes.Usage;
-import org.gradle.api.attributes.java.TargetJvmEnvironment;
-import org.gradle.api.attributes.java.TargetJvmVersion;
 import org.gradle.api.model.ObjectFactory;
 
 import javax.inject.Inject;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.jar.JarInputStream;
 
 // @CacheableTransform
-class LegacyForgeMetadataTransform implements ComponentMetadataRule {
-    private final ObjectFactory objects;
-    private final RepositoryResourceAccessor repositoryResourceAccessor;
-
+class LegacyForgeMetadataTransform extends LegacyMetadataTransform {
     @Inject
     public LegacyForgeMetadataTransform(ObjectFactory objects, RepositoryResourceAccessor repositoryResourceAccessor) {
-        this.objects = objects;
-        this.repositoryResourceAccessor = repositoryResourceAccessor;
+        super(objects, repositoryResourceAccessor);
     }
 
     @Override
     public void execute(ComponentMetadataContext context) {
+        executeWithConfig(context, createPath(context, "userdev", "jar"));
+    }
+
+    @Override
+    public void adaptWithConfig(ComponentMetadataContext context, JsonObject config) {
         var details = context.getDetails();
         var id = details.getId();
 
         var userdevJarName = id.getName() + "-" + id.getVersion() + "-userdev.jar";
-        var userdevJarPath = id.getGroup().replace('.', '/') + "/" + id.getName() + "/" + id.getVersion() + "/" + userdevJarName;
-
-        JsonObject[] configRootHolder = new JsonObject[1];
-        repositoryResourceAccessor.withResource(userdevJarPath, inputStream -> {
-            try (var zin = new JarInputStream(new BufferedInputStream(inputStream))) {
-                for (var entry = zin.getNextJarEntry(); entry != null; entry = zin.getNextJarEntry()) {
-                    if (entry.getName().equals("config.json")) {
-                        var configJson = new String(zin.readAllBytes(), StandardCharsets.UTF_8);
-                        configRootHolder[0] = new Gson().fromJson(configJson, JsonObject.class);
-                    }
-                }
-            } catch (IOException e) {
-                throw new GradleException("Failed to read " + userdevJarPath);
-            }
-        });
-
-        if (configRootHolder[0] == null) {
-            throw new GradleException("Couldn't find userdev config json in " + userdevJarPath);
-        }
 
         Action<DirectDependenciesMetadata> vanillaDependencies = deps -> {
             deps.add("de.oceanlabs.mcp:mcp_config:" + id.getVersion().split("-")[0], DirectDependencyMetadata::endorseStrictVersions);
@@ -90,7 +60,7 @@ class LegacyForgeMetadataTransform implements ComponentMetadataRule {
         });
         details.addVariant("modDevModulePath", variantMetadata -> {
             variantMetadata.withDependencies(dependencies -> {
-                var modules = configRootHolder[0].getAsJsonArray("modules");
+                var modules = config.getAsJsonArray("modules");
                 for (JsonElement module : modules) {
                     dependencies.add(module.getAsString());
                 }
@@ -106,7 +76,7 @@ class LegacyForgeMetadataTransform implements ComponentMetadataRule {
                 attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.class, Usage.JAVA_API));
             });
             variantMetadata.withDependencies(dependencies -> {
-                var libraries = configRootHolder[0].getAsJsonArray("libraries");
+                var libraries = config.getAsJsonArray("libraries");
                 for (JsonElement library : libraries) {
                     dependencies.add(library.getAsString());
                 }
@@ -128,26 +98,15 @@ class LegacyForgeMetadataTransform implements ComponentMetadataRule {
             variantMetadata.withDependencies(vanillaDependencies);
             variantMetadata.withFiles(MutableVariantFilesMetadata::removeAllFiles);
             variantMetadata.withDependencies(dependencies -> {
-                var modules = configRootHolder[0].getAsJsonArray("modules");
+                var modules = config.getAsJsonArray("modules");
                 for (JsonElement module : modules) {
                     dependencies.add(module.getAsString());
                 }
-                var libraries = configRootHolder[0].getAsJsonArray("libraries");
+                var libraries = config.getAsJsonArray("libraries");
                 for (JsonElement library : libraries) {
                     dependencies.add(library.getAsString());
                 }
             });
         });
-//        details.addVariant("universalJar", variantMetadata -> {
-//            variantMetadata.withFiles(files -> {
-//                files.removeAllFiles();
-//                files.addFile(id.getName() + "-" + id.getVersion() + "-universal.jar");
-//            });
-//            variantMetadata.attributes(attributes -> {
-//                attributes.attribute(Category.CATEGORY_ATTRIBUTE, objects.named(Category.class, Category.LIBRARY));
-//                attributes.attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.class, Bundling.EXTERNAL));
-//                attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.class, Usage.JAVA_RUNTIME));
-//            });
-//        });
     }
 }
--- src/legacy/java/net/neoforged/moddevgradle/legacy/McpMetadataTransform.java
+++ src/legacy/java/net/neoforged/moddevgradle/legacy/McpMetadataTransform.java
@@ -1,11 +1,8 @@
 package net.neoforged.moddevgradle.legacy;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.gradle.api.GradleException;
 import org.gradle.api.artifacts.ComponentMetadataContext;
-import org.gradle.api.artifacts.ComponentMetadataRule;
 import org.gradle.api.artifacts.repositories.RepositoryResourceAccessor;
 import org.gradle.api.attributes.Attribute;
 import org.gradle.api.attributes.Usage;
@@ -13,10 +10,6 @@ import org.gradle.api.attributes.java.TargetJvmVersion;
 import org.gradle.api.model.ObjectFactory;
 
 import javax.inject.Inject;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.jar.JarInputStream;
 
 /**
  * Given an implicit Metadata object by Gradle (which results from reading in a pom.xml from Maven for MCP data,
@@ -26,44 +19,27 @@ import java.util.jar.JarInputStream;
  * https://maven.neoforged.net/releases/net/neoforged/neoform/1.21-20240613.152323/neoform-1.21-20240613.152323.module
  */
 // @CacheableTransform
-class McpMetadataTransform implements ComponentMetadataRule {
+class McpMetadataTransform extends LegacyMetadataTransform {
     private static final Attribute<String> JVM_VERSION = Attribute.of("org.gradle.jvm.version", String.class);
-    private final ObjectFactory objects;
-    private final RepositoryResourceAccessor repositoryResourceAccessor;
 
     @Inject
     public McpMetadataTransform(ObjectFactory objects, RepositoryResourceAccessor repositoryResourceAccessor) {
-        this.objects = objects;
-        this.repositoryResourceAccessor = repositoryResourceAccessor;
+        super(objects, repositoryResourceAccessor);
     }
 
     @Override
     public void execute(ComponentMetadataContext context) {
+        executeWithConfig(context, createPath(context, "", "zip"));
+    }
+
+    @Override
+    protected void adaptWithConfig(ComponentMetadataContext context, JsonObject config) {
         var details = context.getDetails();
         var id = details.getId();
 
         var zipDataName = id.getName() + "-" + id.getVersion() + ".zip";
-        var zipDataPath = id.getGroup().replace('.', '/') + "/" + id.getName() + "/" + id.getVersion() + "/" + zipDataName;
 
-        JsonObject[] configRootHolder = new JsonObject[1];
-        repositoryResourceAccessor.withResource(zipDataPath, inputStream -> {
-            try (var zin = new JarInputStream(new BufferedInputStream(inputStream))) {
-                for (var entry = zin.getNextJarEntry(); entry != null; entry = zin.getNextJarEntry()) {
-                    if (entry.getName().equals("config.json")) {
-                        var configJson = new String(zin.readAllBytes(), StandardCharsets.UTF_8);
-                        configRootHolder[0] = new Gson().fromJson(configJson, JsonObject.class);
-                    }
-                }
-            } catch (IOException e) {
-                throw new GradleException("Failed to read " + zipDataPath);
-            }
-        });
-
-        if (configRootHolder[0] == null) {
-            throw new GradleException("Couldn't find userdev config json in " + zipDataPath);
-        }
-
-        var javaTarget = configRootHolder[0].getAsJsonPrimitive("java_target").getAsInt();
+        var javaTarget = config.getAsJsonPrimitive("java_target").getAsInt();
 
         // a.k.a. "neoformData"
         // Primarily pulled to use for NFRT manifest
@@ -76,7 +52,7 @@ class McpMetadataTransform implements ComponentMetadataRule {
             });
             // Add tools required by this version of MCP as dependencies of this variant
             variantMetadata.withDependencies(dependencies -> {
-                var functions = configRootHolder[0].getAsJsonObject("functions");
+                var functions = config.getAsJsonObject("functions");
                 for (var function : functions.entrySet()) {
                     var toolCoordinate = ((JsonObject) function.getValue()).getAsJsonPrimitive("version").getAsString();
                     dependencies.add(toolCoordinate);
@@ -108,7 +84,7 @@ class McpMetadataTransform implements ComponentMetadataRule {
                 attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage.class, Usage.JAVA_API));
             });
             variantMetadata.withDependencies(dependencies -> {
-                var libraries = configRootHolder[0].getAsJsonObject("libraries").getAsJsonArray("joined");
+                var libraries = config.getAsJsonObject("libraries").getAsJsonArray("joined");
                 for (JsonElement library : libraries) {
                     dependencies.add(library.getAsString());
                 }
