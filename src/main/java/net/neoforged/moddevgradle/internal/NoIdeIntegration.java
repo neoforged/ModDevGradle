b0eff1835cab592292baf0f0754a5d1141e8ebbf
shartte <shartte@users.noreply.github.com>
2024-10-14 10:49:04 +0200

    Fix synchronisation for Eclipse/VSCode (#174)

--- src/main/java/net/neoforged/moddevgradle/internal/NoIdeIntegration.java
+++ src/main/java/net/neoforged/moddevgradle/internal/NoIdeIntegration.java
@@ -1,7 +1,6 @@
 package net.neoforged.moddevgradle.internal;
 
 import org.gradle.api.Project;
-import org.gradle.api.tasks.TaskProvider;
 
 /**
  * This implementation of {@link IdeIntegration} is used when no IDE was detected to host Gradle.
@@ -9,10 +8,5 @@ import org.gradle.api.tasks.TaskProvider;
 final class NoIdeIntegration extends IdeIntegration {
     public NoIdeIntegration(Project project) {
         super(project);
-    }
-
-    @Override
-    protected void registerProjectSyncTask(TaskProvider<?> task) {
-        // No IDE
     }
 }
