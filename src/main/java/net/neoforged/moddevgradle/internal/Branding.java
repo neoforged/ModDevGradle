b068b2af9d2c9289bfd7d0fd8db393b5dbc5ac49
Sebastian Hartte <shartte@users.noreply.github.com>
2024-11-23 01:57:59 +0100

    Make package level visible.

--- src/main/java/net/neoforged/moddevgradle/internal/Branding.java
+++ src/main/java/net/neoforged/moddevgradle/internal/Branding.java
@@ -6,7 +6,7 @@ package net.neoforged.moddevgradle.internal;
  * @param publicTaskGroup   Use this group for tasks that are considered to be part of the user-interface of MDG.
  * @param internalTaskGroup Use this group for tasks that are considered to be an implementation detail of MDG.
  */
-public record Branding(String publicTaskGroup, String internalTaskGroup) {
+record Branding(String publicTaskGroup, String internalTaskGroup) {
     public static final Branding MDG = new Branding("mod development", "mod development/internal");
     public static final Branding NEODEV = new Branding("neoforge development", "neoforge development/internal");
 }
