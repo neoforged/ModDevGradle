44ecd8f56053cf406ddb50f9d6ecc6be49a5ed0b
Sebastian Hartte <shartte@users.noreply.github.com>
2024-11-27 00:43:13 +0100

    Fix compilation issue

--- testproject/jijtest/src/test/java/jijtest/CoreModTest.java
+++ testproject/jijtest/src/test/java/jijtest/CoreModTest.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.ItemStack;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CoreModTest {
