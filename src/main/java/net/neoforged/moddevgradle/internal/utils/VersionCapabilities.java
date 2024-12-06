e72938b9f0f15e2265ee2800798d2cfa58a9e3e5
Sebastian Hartte <shartte@users.noreply.github.com>
2024-12-06 13:53:26 +0100

    WIP

--- src/main/java/net/neoforged/moddevgradle/internal/utils/VersionCapabilities.java
+++ src/main/java/net/neoforged/moddevgradle/internal/utils/VersionCapabilities.java
@@ -18,19 +18,17 @@ public record VersionCapabilities(int javaVersion, boolean splitDataRuns) {
     private static final int MC_1_18_PRE2_INDEX = getReferenceVersionIndex("1.18-pre2");
     private static final int MC_21W19A_INDEX = getReferenceVersionIndex("21w19a");
 
-    /**
-     * Minecraft version optionally followed by a separator (. or -) and other information.
-     * This will parse 1.21.2-rc as 1.21.2
-     */
-    private static final Pattern LEADING_RELEASE_PATTERN = Pattern.compile("^(\\d+\\.\\d+)\\.\\d+(|[.-].*)$");
-
     public static VersionCapabilities ofMinecraftVersion(String minecraftVersion) {
-        var versionIndex = getReferenceVersionIndex(minecraftVersion);
+        var versionIndex = MinecraftVersionList.VERSIONS.indexOf(minecraftVersion);
         if (versionIndex == -1) {
             LOG.info("Minecraft Version {} is unknown. Assuming latest capabilities.", versionIndex);
             return LATEST;
         }
 
+        return ofVersionIndex(versionIndex);
+    }
+
+    public static VersionCapabilities ofVersionIndex(int versionIndex) {
         var javaVersion = getJavaVersion(versionIndex);
         var splitData = hasSplitDataEntrypoints(versionIndex);
 
@@ -38,11 +36,11 @@ public record VersionCapabilities(int javaVersion, boolean splitDataRuns) {
     }
 
     static int getJavaVersion(int versionIndex) {
-        if (versionIndex >= MC_24W14A_INDEX) {
+        if (versionIndex <= MC_24W14A_INDEX) {
             return 21;
-        } else if (versionIndex >= MC_1_18_PRE2_INDEX) {
+        } else if (versionIndex <= MC_1_18_PRE2_INDEX) {
             return 17;
-        } else if (versionIndex >= MC_21W19A_INDEX) {
+        } else if (versionIndex <= MC_21W19A_INDEX) {
             return 16;
         } else {
             return 8;
@@ -50,34 +48,56 @@ public record VersionCapabilities(int javaVersion, boolean splitDataRuns) {
     }
 
     static boolean hasSplitDataEntrypoints(int versionIndex) {
-        return versionIndex >= MC_24W45A_INDEX;
+        return versionIndex <= MC_24W45A_INDEX;
     }
 
-    public static VersionCapabilities ofNeoForgeVersion(String version) {
+    static int indexOfNeoForgeVersion(String version) {
         // NeoForge omits the "1." at the start of the Minecraft version and just adds an incrementing last digit
         var matcher = NEOFORGE_PATTERN.matcher(version);
         if (!matcher.matches()) {
+            return -1;
+        }
+        return MinecraftVersionList.VERSIONS.indexOf("1." + matcher.group(1));
+    }
+
+    public static VersionCapabilities ofNeoForgeVersion(String version) {
+        var index = indexOfNeoForgeVersion(version);
+        if (index == -1) {
             LOG.warn("Failed to parse MC version from NeoForge version {}. Using capabilities of latest known Minecraft version.", version);
             return LATEST;
         }
 
-        var minecraftVersion = "1." + matcher.group(1);
+        return ofVersionIndex(index);
+    }
 
-        return ofMinecraftVersion(minecraftVersion);
+    static int indexOfNeoFormVersion(String version) {
+        // Examples: 1.21-<timestamp>
+        return MinecraftVersionList.indexOfByPrefix(version, "-");
     }
 
     public static VersionCapabilities ofNeoFormVersion(String version) {
-        var matcher = LEADING_RELEASE_PATTERN.matcher(version);
-        if (matcher.matches()) {
-            return ofMinecraftVersion(matcher.group(1));
+        var index = indexOfNeoFormVersion(version);
+        if (index == -1) {
+            LOG.warn("Failed to parse MC version from NeoForm version {}. Using capabilities of latest known Minecraft version.", version);
+            return LATEST;
         }
 
-        LOG.warn("Failed to parse MC version from NeoForm version {}. Using capabilities of latest known Minecraft version.", version);
-        return LATEST;
+        return ofVersionIndex(index);
     }
 
-    public static VersionCapabilities ofForgeVersion(String forgeVersion) {
-        return ofMinecraftVersion(forgeVersion);
+    static int indexOfForgeVersion(String version) {
+        // Forge versions are generally "<mc-version>-<forge-version-and-stuff>"
+        return MinecraftVersionList.indexOfByPrefix(version, "-");
+    }
+
+    public static VersionCapabilities ofForgeVersion(String version) {
+        var index = indexOfForgeVersion(version);
+        if (index == -1) {
+            LOG.warn("Failed to parse MC version from Forge version {}. Using capabilities of latest known Minecraft version.", version);
+            return LATEST;
+        }
+
+        return ofVersionIndex(index);
     }
 
     private static int getReferenceVersionIndex(String v) {
