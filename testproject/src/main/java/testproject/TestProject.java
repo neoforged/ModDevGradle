ee9b01ee59c8c231442e7ee2d8c50c0a84085ce9
Technici4n <13494793+Technici4n@users.noreply.github.com>
2025-11-07 00:05:37 +0100

    Update testproject to 1.21.10
    Co-authored-by: Sebastian Hartte <shartte@users.noreply.github.com>

--- testproject/src/main/java/testproject/TestProject.java
+++ testproject/src/main/java/testproject/TestProject.java
@@ -11,8 +11,8 @@ import subproject.SubProject;
 @Mod("testproject")
 public class TestProject {
     public TestProject() {
-        System.out.println(DetectedVersion.tryDetectVersion().getName());
-        System.out.println("Top-Level: " + ((DetectedVersion) DetectedVersion.BUILT_IN).buildTime);
+        System.out.println(DetectedVersion.tryDetectVersion().id());
+        System.out.println("Top-Level: " + DetectedVersion.LOGGER.getName());
         System.out.println(SubProject.class.getName());
 
         new ApiTest(); // access something from the api source set
