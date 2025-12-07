bcced8b3b49dfd4057e2f98edad27c25fbeb6af6
vfyjxf_ <2331007009@qq.com>
2025-12-07 20:34:03 +0800

    don't write pom to legacyClassPath

--- src/main/java/net/neoforged/moddevgradle/internal/WriteLegacyClasspath.java
+++ src/main/java/net/neoforged/moddevgradle/internal/WriteLegacyClasspath.java
@@ -26,6 +26,7 @@ abstract class WriteLegacyClasspath extends DefaultTask {
             // Use a provider indirection to remove task dependencies.
             // Use file names only.
             return files.getFiles().stream()
+                    .filter(file -> !org.codehaus.plexus.util.FileUtils.extension(file.getName()).equals("pom"))
                     .map(File::getAbsolutePath)
                     .toList();
         }));
