f0f011f4311e5c31d65b4e1c6db32324971e2b3c
Technici4n <13494793+Technici4n@users.noreply.github.com>
2025-12-11 21:15:41 +0100

    Bump NFRT to fix issue

--- src/main/java/net/neoforged/nfrtgradle/NeoFormRuntimeExtension.java
+++ src/main/java/net/neoforged/nfrtgradle/NeoFormRuntimeExtension.java
@@ -12,7 +12,7 @@ import org.gradle.api.provider.Property;
 public abstract class NeoFormRuntimeExtension {
     public static final String NAME = "neoFormRuntime";
 
-    private static final String DEFAULT_NFRT_VERSION = "2.0.3";
+    private static final String DEFAULT_NFRT_VERSION = "2.0.5";
 
     @Inject
     public NeoFormRuntimeExtension(Project project) {
